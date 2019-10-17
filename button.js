4dcf0325e894dc9cd9525c3e3761acd5e7747d07
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2019-10-16 22:24:52 -0500

    Split up toggle

--- button.js
+++ button.js
@@ -1,35 +1,36 @@
-class ToggleButton {
+class KeyedButton {
 	constructor(paren, label, keys) {
-		//this.box = document.createElement("input");
-		//this.box.hidden = "true";
-		//this.box.type = "checkbox";
-		//this.box.className = "toggle-box";
-		this.checked = false;
-		
 		this.button = document.createElement("button");
-		this.button.className = "flex-elem toggle-button";
+		this.button.classList.add("keyed-button");
 		this.button.innerHTML = label;
 		if(keys)
 			this.button.title = `Press ${keys.join(" or ")} to toggle`;
 		
 		this.keys = new Set(keys);
-		this.button.addEventListener("click", this.handle.bind(this));
 		document.body.addEventListener("keydown", this.hkey.bind(this));
-		//paren.appendChild(this.box);
 		paren.appendChild(this.button);
-	}
-	handle(ev) {
-		//this.box.checked = !this.box.checked;
-		this.checked = !this.checked;
-		this.button.style.borderStyle = this.checked ? 'inset' : 'outset';
 	}
 	hkey(ev) {
 		if(!ev.target.parentNode.contains(this.button))
 			return;
 		if(this.keys.has(ev.key))
-			this.handle(ev);
+			this.button.click();
 	}
 }
+class ToggleButton extends KeyedButton {
+	constructor(paren, label, keys) {
+		super(paren, label, keys);
+		this.checked = false;
+		this.button.classList.add("toggle-button");
+		this.button.addEventListener("click", this.handle.bind(this));
+	}
+	handle(ev) {
+		this.checked = !this.checked;
+		this.button.style.borderStyle = this.checked ? 'inset' : 'outset';
+	}
+}
+
+
 window.addEventListener("DOMContentLoaded", () => {
 	const bc = new BuzzCount();
 	this.buttons = [];
@@ -44,13 +45,20 @@ window.addEventListener("DOMContentLoaded", () => {
 	}
 	const paren = document.querySelector(".inner-word-box");
 	for(let i=0; i<bc.all.length; i++) {
-		const letter = firstletter(bc.all[i]);
-		const word = bc.all[i].toUpperCase();
+		const buzz = bc.all[i] || 'fail';
+		const letter = firstletter(buzz);
+		const word = buzz.toUpperCase();
 		const hl = !~letter ? word :
 			`${word.slice(0, letter)}<u>${word[letter]}</u>${word.slice(letter + 1)}`;
 		this.buttons.push(new ToggleButton(
 			paren, hl,
-			[(i + 1).toString(), !~letter ? null : bc.all[i][letter]]));
+			[(i + 1).toString(), buzz[letter]]));
+		this.buttons[i].button.classList.add("flex-elem");
+		this.buttons[i].button.style.backgroundColor = `hsl(${Styler.hueByIndex(i)}, 40%, 90%)`;
+		console.log(Styler.hueByIndex(i));
 	}
+	const goword = "go!";
+	const go = new KeyedButton(document.querySelector('.flex-container'), goword.toUpperCase(), ["Enter", goword[firstletter(goword)]]);
+	go.button.classList.add("flex-elem");
 
 })
--- styler.js
+++ styler.js
@@ -1,6 +1,6 @@
-class Styling {
-	PHI = .5 + Math.sqrt(5) / 2;
-	hueByIndex(index) {
-		return parseInt(Math.pow(PHI, index) % 1.0 * 360);
+class Styler {
+	static get PHI() {return -.5 + Math.sqrt(5) / 2;}
+	static hueByIndex(index) {
+		return parseInt((Styler.PHI * index) % 1.0 * 360);
 	}
 }
