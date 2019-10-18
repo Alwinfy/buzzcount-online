42bcf5e44077c9e48a4f88a8f5c2ae846e1de795
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2019-10-18 16:07:37 -0500

    Moved stuff to main file

--- button.js
+++ button.js
@@ -28,37 +28,8 @@ class ToggleButton extends KeyedButton {
 		this.checked = !this.checked;
 		this.button.style.borderStyle = this.checked ? 'inset' : 'outset';
 	}
+	reset() {
+		this.checked = true;
+		this.handle(null);
+	}
 }
-
-
-window.addEventListener("DOMContentLoaded", () => {
-	const bc = new BuzzCount();
-	this.buttons = [];
-	const letters = new Set();
-	const firstletter = word => {
-		for(let i=0; i<word.length; i++)
-			if(!letters.has(word[i])) {
-				letters.add(word[i]);
-				return i;
-			}
-		return -1;
-	}
-	const paren = document.querySelector(".inner-word-box");
-	for(let i=0; i<bc.all.length; i++) {
-		const buzz = bc.all[i] || 'fail';
-		const letter = firstletter(buzz);
-		const word = buzz.toUpperCase();
-		const hl = !~letter ? word :
-			`${word.slice(0, letter)}<u>${word[letter]}</u>${word.slice(letter + 1)}`;
-		this.buttons.push(new ToggleButton(
-			paren, hl,
-			[(i + 1).toString(), buzz[letter]]));
-		this.buttons[i].button.classList.add("flex-elem");
-		this.buttons[i].button.style.backgroundColor = `hsl(${Styler.hueByIndex(i)}, 40%, 90%)`;
-		console.log(Styler.hueByIndex(i));
-	}
-	const goword = "go!";
-	const go = new KeyedButton(document.querySelector('.flex-container'), goword.toUpperCase(), ["Enter", goword[firstletter(goword)]]);
-	go.button.classList.add("flex-elem");
-
-})
